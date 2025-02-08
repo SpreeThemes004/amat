41394f387e23f0b2d1466bc7a378d65a8c9f11e2
rhsrasel <rhsrasel004@gmail.com>
2025-02-08 23:59:49 +0600

    upload amat v1

---
+++ html/assets/js/main.js
@@ -0,0 +1,707 @@
+// aos
+AOS.init({
+  duration: 1500,
+});
+
+/* YOUTUBE VIDEO PLAYER START
+=> Load the YouTube IFrame Player API code asynchronously.
+=> https://developers.google.com/youtube/iframe_api_reference
+*/
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+document.body.appendChild(tag);
+
+/* When the YouTube API code loads, it calls this function, so it must be global and it must be named exactly onYouTubeIframeAPIReady. */
+window.onYouTubeIframeAPIReady = function () {
+  var videoModules = document.querySelectorAll('.video-main');
+  if(!videoModules) return;
+  // for Internet Explorer 11 and below, convert array-like NodeList to an actual Array.
+  videoModules = Array.prototype.slice.call(videoModules);
+  videoModules.forEach(initializeVideoModule);
+}
+
+function initializeVideoModule(videoModule){
+  var player = new YT.Player(videoModule.querySelector('.video-placeholder'), {
+    videoId: videoModule.dataset.videoId,
+    events: {
+      onStateChange: function (event) {
+        var isEnded = event.data === YT.PlayerState.ENDED;
+        // 'playing' css class controls fading the video and preivew images in/out.
+        // Internet Explorer 11 and below do not support a second argument to `toggle`
+        // videoModule.classList.toggle('playing', !isEnded);
+        videoModule.classList[isEnded ? 'remove' : 'add']('playing');
+        // if the video is done playing, remove it and re-initialize
+        if(isEnded){
+          player.destroy();
+          videoModule.querySelector('.video-layer').innerHTML = (
+            '<div class="video-placeholder"></div>'
+          );
+          initializeVideoModule(videoModule);
+        }
+      }
+    }
+  });
+}
+/* YOUTUBE VIDEO PLAYER END */
+
+
+// mobile submenu
+class MobileSubmenu extends HTMLElement {
+  constructor(){
+    super();
+
+    this.opener = this.querySelectorAll('.open-submenu');
+    this.btnBack = this.querySelectorAll('.btn-menu-back');
+
+    this.opener.forEach(item => {
+      item.addEventListener('click', this.open);
+    })
+
+    this.btnBack.forEach(item => {
+      item.addEventListener('click', this.close);
+    })
+  }
+
+  open(event) {
+    event.target.parentElement.parentElement.querySelector('.submenu-transform').classList.add('active');
+  }
+
+  close(event) {
+    event.target.closest('.submenu-transform').classList.remove('active');
+  }
+}
+
+customElements.define('mobile-submenu', MobileSubmenu);
+
+// header search
+class HeaderSearch extends HTMLElement{
+  constructor(){
+    super();
+
+    this.searchWrapper = this.closest('header').querySelector('.search-wrapper');
+    this.searchBtn = this.querySelector('.icon-search');
+    this.closeBtn = this.closest('header').querySelector('.search-close');
+
+    this.searchBtn.addEventListener('click', this.toggle.bind(this));
+    this.closeBtn.addEventListener('click', this.toggle.bind(this));
+  }
+
+  toggle(){
+    this.searchWrapper.classList.toggle('search-appear');
+  }
+}
+
+customElements.define('header-search', HeaderSearch);
+
+// Hero slider
+class HeroSlider extends HTMLElement{
+  constructor(){
+    super();
+
+    this.swiper = this.querySelector('.swiper');
+    this.sliderPagination = this.querySelector('.swiper-pagination');
+    this.navNext = this.querySelector('.arrow-next');
+    this.navPrev = this.querySelector('.arrow-prev');
+
+    this.init();
+  }
+
+  init(){
+    this.slider = new Swiper(this.swiper, {
+      loop: true,
+      navigation: {
+        nextEl: this.navNext,
+        prevEl: this.navPrev,
+      },
+      pagination: {
+        el: this.sliderPagination,
+        clickable: true,
+      },
+    });
+  }
+}
+
+customElements.define('hero-slider', HeroSlider)
+
+
+// Accordian 
+class Accordion extends HTMLElement{
+  constructor(){
+    super();
+
+    this.opener = this.querySelectorAll('.accordion-opener');
+    this.faqOpener = this.querySelectorAll('.faq-header');
+
+    this.opener.forEach(item => {
+      item.addEventListener('click', this.toggleMenu.bind(this));
+    })
+
+    // for faq page only
+    this.faqOpener.forEach(item => {
+      item.addEventListener('click', this.toggleFaq.bind(this));
+    })
+  }
+
+  toggleMenu(event) {
+    event.target.nextElementSibling.classList.toggle('open');   
+  }
+
+  // for faq page only
+  toggleFaq(event) {
+    event.target.classList.toggle('minus');      
+  }
+} 
+
+customElements.define('accordion-menu', Accordion)
+
+// Cart drawer increament-decreament input
+class QuantityInput extends HTMLElement {
+  constructor() {
+    super();
+    this.input = this.querySelector('input');
+    this.querySelectorAll('button').forEach((button) =>
+      button.addEventListener('click', this.onButtonClick.bind(this))
+    );
+  }
+
+  onButtonClick(event) {
+    event.preventDefault();
+    if (event.target.name === 'plus') {      
+      this.input.stepUp();
+    } else {
+      this.input.stepDown();
+    }
+  }
+}
+
+customElements.define('quantity-input', QuantityInput);
+
+// Modal openar
+class ModalOpener extends HTMLElement {
+  constructor() {
+    super();
+
+    const button = this.querySelector('button');
+
+    if (!button) return;
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(this.getAttribute('data-modal'));
+      if (modal) modal.show(button);
+    });
+  }
+
+}
+customElements.define('modal-opener', ModalOpener);
+
+// Modal dialog
+class ModalDialog extends HTMLElement {
+  constructor() {
+    super();
+
+    this.querySelector('.product-popup-modal__toggle').addEventListener('click', this.hide.bind(this, false));
+
+    this.media = this.querySelector('.video');
+
+    this.addEventListener('keyup', (event) => {
+      if (event.code.toUpperCase() === 'ESCAPE') this.hide();
+    });
+    if (this.classList.contains('media-modal')) {
+      this.addEventListener('pointerup', (event) => {
+        if (event.pointerType === 'mouse' && !event.target.closest('deferred-media, product-model')) this.hide();
+      });
+    } else {
+      this.addEventListener('click', (event) => {
+        if (event.target === this) this.hide();
+      });
+    }
+  }
+
+  connectedCallback() {
+    if (this.moved) return;
+    this.moved = true;
+    document.body.appendChild(this);
+  }
+
+  show(opener) {
+    this.openedBy = opener;
+    const popup = this.querySelector('.template-popup');
+    document.body.classList.add('overflow-hidden');
+    this.setAttribute('open', '');
+    if (popup) {
+      popup.loadContent();
+      this.playMedia();
+    };
+  }
+
+  hide() {
+    document.body.classList.remove('overflow-hidden');
+    document.body.dispatchEvent(new CustomEvent('modalClosed'));
+    this.removeAttribute('open');
+    this.pauseMedia();
+  }
+
+  playMedia() {
+    this.querySelectorAll('.js-youtube').forEach((video) => {
+      video.contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
+    });
+    this.querySelectorAll('.js-vimeo').forEach((video) => {
+      video.contentWindow.postMessage('{"method":"play"}', '*');
+    });
+    this.querySelectorAll('video').forEach((video) => video.play());
+  }
+
+  pauseMedia() {
+    this.querySelectorAll('.js-youtube').forEach((video) => {
+      video.contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+    });
+    this.querySelectorAll('.js-vimeo').forEach((video) => {
+      video.contentWindow.postMessage('{"method":"pause"}', '*');
+    });
+    this.querySelectorAll('video').forEach((video) => video.pause());
+  }
+}
+
+customElements.define('modal-dialog', ModalDialog);
+
+// Product details slider
+class productSlider extends HTMLElement{
+  constructor(){
+    super()
+
+    this.swiperLarge = this.querySelector('.swiper-large');
+    this.swiperThumb = this.querySelector('.swiper-thumb');
+
+    this.navNextThumb = this.querySelector('.nav-thumb .swiper-button-next');
+    this.navPrevThumb = this.querySelector('.nav-thumb .swiper-button-prev');
+
+    this.direction = this.swiperThumb.dataset.direction;
+    this.vertical = null;
+   
+
+
+    if(this.direction == 'vertical') {
+      this.vertical = {
+        768: {
+          direction: 'vertical'
+        }
+      }
+    }
+
+    this.init();
+
+    const resizeObserver = new ResizeObserver((entries) => this.init());
+    resizeObserver.observe(this);
+  }
+
+  init(){
+    this.sliderThumb = new Swiper(this.swiperThumb, {
+      spaceBetween: 8,
+      slidesPerView: 4,
+      loop: true,
+      freeMode: true,
+      watchSlidesProgress: true,
+      autoHeight: true,
+      navigation: {
+        nextEl: this.navNextThumb,
+        prevEl: this.navPrevThumb,
+      },
+      breakpoints: this.vertical
+    });
+
+    this.sliderLarge  = new Swiper(this.swiperLarge, {
+      spaceBetween: 10,
+      loop: true,
+      navigation: {
+        nextEl: this.navNextLarge,
+        prevEl: this.navPrevLarge,
+      },
+      thumbs: {
+        swiper: this.sliderThumb,
+      },
+    });
+  }
+}
+
+customElements.define('product-details-slider', productSlider);
+
+// Product slider
+class newProductSlider extends HTMLElement{
+  constructor(){
+    super();
+
+    this.productSwiper = this.querySelector('.swiper');
+    this.navNext = this.querySelector('.product-nav-next');
+    this.navPrev = this.querySelector('.product-nav-prev');
+    this.sliderPagination = this.querySelector('.swiper-pagination');
+
+    this.slidesPerView = 1,
+    this.breakpoints =  {
+                          320: {
+                            slidesPerView: 1.8,
+                            spaceBetween: 16,
+                          },
+                          575: {
+                            slidesPerView: 2.6,
+                            spaceBetween: 16,
+                          },
+                          768: {
+                            slidesPerView: 3,
+                            spaceBetween: 24,
+                          },
+                          992: {
+                            slidesPerView: 4,
+                            spaceBetween: 24,
+                          },
+                        }
+                  
+    if(this.getAttribute('data-slidesPerView')){
+      this.slidesPerView = parseFloat(this.getAttribute('data-slidesPerView'));
+      this.breakpoints ={
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 24,
+        },
+        768: {
+          slidesPerView: 2.8,
+          spaceBetween: 24,
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 24,
+        }
+      };
+    }
+
+    this.init();
+  }
+
+  init(){
+    this.productSlider = new Swiper(this.productSwiper, {
+      slidesPerView: this.slidesPerView,
+      spaceBetween: 24,
+      navigation: {
+        nextEl: this.navNext,
+        prevEl: this.navPrev,
+      },
+      breakpoints: this.breakpoints,
+    });
+  }
+}
+
+customElements.define('product-slider', newProductSlider);
+
+// Category slider
+class categotySlider extends HTMLElement{
+  constructor(){
+    super();
+
+    this.categorySwiper = this.querySelector('.swiper');
+    this.navNext = this.querySelector('.product-nav-next');
+    this.navPrev = this.querySelector('.product-nav-prev');
+    this.sliderPagination = this.querySelector('.swiper-pagination');
+
+    this.init();
+  }
+
+  init(){
+    this.categorySlider = new Swiper(this.categorySwiper, {
+      slidesPerView: this.slidesPerView,
+      spaceBetween: 24,
+      navigation: {
+        nextEl: this.navNext,
+        prevEl: this.navPrev,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1.5,
+          spaceBetween: 16,
+        },
+        575: {
+          slidesPerView: 2,
+          spaceBetween: 16,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 24,
+        },
+        992: {
+          slidesPerView: 5,
+          spaceBetween: 24,
+        }
+      }
+    });
+  }
+}
+
+customElements.define('category-slider', categotySlider);
+
+// Testmonial slider 
+class testimonialSlider extends HTMLElement{
+  constructor(){
+    super();
+
+    this.swiper = this.querySelector('.swiper');
+    this.testimonialNext = this.querySelector('.testimonial-next')
+    this.testimonialprev = this.querySelector('.testimonial-prev')
+
+    this.slidesPerView = 1;
+    this.breakpoints ={
+                        0: {
+                          slidesPerView: 1,
+                        }
+                      };
+    if(this.getAttribute('data-slidesPerView')){
+      this.slidesPerView = parseFloat(this.getAttribute('data-slidesPerView'));
+      this.breakpoints ={
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 24,
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 24,
+        }
+      };
+    }
+
+    this.init();
+  }
+
+  init(){
+    this.slider = new Swiper(this.swiper, {
+      slidesPerView: this.slidesPerView,
+      spaceBetween: 24,
+      loop: true,
+      navigation: {
+        nextEl: this.testimonialNext,
+        prevEl: this.testimonialprev,
+      },
+      breakpoints: this.breakpoints,
+    });
+    
+  }
+}
+
+customElements.define('testimonial-slider', testimonialSlider);
+
+
+// Instagram slider
+class instagramSlider extends HTMLElement{
+  constructor(){
+    super();
+
+    this.instagramSwiper = this.querySelector('.swiper');
+    this.navNext = this.querySelector('.insta-nav-next');
+    this.navPrev = this.querySelector('.insta-nav-prev');
+
+    this.init();
+  }
+
+  init(){
+    this.instagramSlider = new Swiper(this.instagramSwiper, {
+      slidesPerView: this.slidesPerView,
+      spaceBetween: 0,
+      navigation: {
+        nextEl: this.navNext,
+        prevEl: this.navPrev,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1.8,
+        },
+        575: {
+          slidesPerView: 2.8,
+        },
+        768: {
+          slidesPerView: 3.5,
+        },
+        992: {
+          slidesPerView: 5,
+        }
+      }
+    });
+  }
+}
+
+customElements.define('instagram-slider', instagramSlider);
+
+
+// counter up 
+class CounterUp extends HTMLElement{
+  constructor(){
+    super();
+
+    this.counterList = this.querySelector('.counter-up-list');
+    this.activated = false;
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      if (!entries[0].isIntersecting) return;
+      observer.unobserve(this.counterList);
+      this.updateCounter();
+    });
+    
+    observer.observe(this.counterList);
+  }
+
+  updateCounter(){
+    this.counters = this.querySelectorAll('.count-number');
+
+    this.counters.forEach(counter =>{
+      const speed = 200;
+    
+      const upCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const value = +counter.innerText;
+        const increament = target / speed;
+
+        if(value < target){
+          counter.innerText = Math.ceil(value + increament);
+          setTimeout(upCount, 2000 / target)
+        }else{
+          counter.innerText = value;
+        }
+      }
+      upCount();
+    })
+  }
+  
+}
+
+customElements.define('counter-up', CounterUp);
+
+// Scroll up button
+class scrollUp extends HTMLElement{
+  constructor(){
+    super();
+
+    window.onscroll = () => {
+      this.scrollUpFunc();
+    };
+
+    window.onload = () => {
+      this.scrollUpFunc();
+    }; 
+  }
+
+  scrollUpFunc(){
+    let srcollUpDiv = document.querySelector('#scroll-up-wrap');
+    let position = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollVal = Math.round((position * 100) / calcHeight);
+    if(position > 100) {
+      srcollUpDiv.classList.add('show');
+    } else {
+      srcollUpDiv.classList.remove('show');
+    };
+
+    srcollUpDiv.addEventListener('click', () => {
+      document.documentElement.scrollTop = 0;
+    });
+    srcollUpDiv.style.background =`conic-gradient(#000 ${scrollVal}%, #F7F7F7 ${scrollVal}%)`;
+  }
+}
+
+customElements.define('scroll-up', scrollUp);
+
+// Newsletter popup
+class modalPopUp extends HTMLElement{
+  constructor(){
+    super();
+    
+    this.popup = document.querySelector('.newsletter-popup');
+    this.overlay = this.querySelector('.overlay-popup');
+    this.closeButton = this.querySelector('.product-popup-modal__toggle');
+
+    window.addEventListener('load', this.openPopup.bind(this));
+    window.addEventListener('keydown', this.escapeBtn.bind(this));
+
+    this.closeButton.addEventListener('click', this.closePopup.bind(this));
+    this.overlay.addEventListener('click', this.closePopup.bind(this));
+
+    this.popup.addEventListener('wheel', function(event){
+      event.preventDefault();
+    }, false);    
+  }
+
+  openPopup(){
+    setTimeout(() => {
+      this.popup.classList.remove('hidden');
+      this.overlay.classList.remove('hidden');
+    }, 2000);
+  };
+
+  closePopup(){
+    this.popup.classList.add('hidden');
+    this.overlay.classList.add('hidden');
+  };
+
+  escapeBtn(e){
+    if( e.key === 'Escape' && !this.popup.classList.contains('hidden')){
+      this.popup.classList.add('hidden');
+      this.overlay.classList.add('hidden');
+    }
+  };
+}
+
+customElements.define('modal-popup', modalPopUp);
+
+// Count Down
+class countDown extends HTMLElement{
+  constructor(){
+    super();
+    
+    window.addEventListener('load', this.countFunc.bind(this));
+  }
+
+  countFunc(){
+    this.countIntervel = setInterval (() => {
+      this.countDownDate = new Date("jan 1, 2026 12:00:00").getTime();
+      this.now = new Date().getTime();
+      this.distance = this.countDownDate - this.now;
+
+      this.days = Math.floor(this.distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((this.distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((this.distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((this.distance % (1000 * 60)) / 1000);
+
+      this.querySelector('.count-days').innerHTML = this.days;
+      this.querySelector('.count-hours').innerHTML = this.hours;
+      this.querySelector('.count-min').innerHTML = this.minutes;
+      this.querySelector('.count-sec').innerHTML = this.seconds;
+
+      if (this.distance < 0) {
+        clearInterval(this.countIntervel);
+      }
+    }, 1000);
+  }
+}
+
+customElements.define('count-down', countDown);
+
+// Filter drawer
+
+class filterDrawer extends HTMLElement {
+  constructor() {
+    super();
+
+    this.filterWrapper = this.querySelector(".filter-sidebar");
+    this.filterButtons = this.querySelectorAll(".filter-opener");
+
+    this.filterButtons.forEach(btn => {
+      btn.addEventListener('click', this.filterToggle.bind(this));
+    });
+  }
+
+  filterToggle(){
+    this.filterWrapper.classList.toggle("open-sidebar");
+  }
+}
+
+
+
+customElements.define('filter-drawer', filterDrawer);
+
+
